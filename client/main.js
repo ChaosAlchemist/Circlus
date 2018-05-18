1f4b87c24a3086d09b7d351f85df40b75283ae0d
Chaos <jrcortest1@gmail.com>
2018-05-18 13:35:43 -0400

    Integración con MongoDB

--- client/main.js
+++ client/main.js
@@ -1,3 +1,11 @@
 import { Template } from 'meteor/templating';
+import { Temas } from '../lib/collections.js';
 
 import './main.html';
+
+// Importar temas
+Template.body.helpers({
+  temas(){
+    return Temas.find({});
+  }
+});
